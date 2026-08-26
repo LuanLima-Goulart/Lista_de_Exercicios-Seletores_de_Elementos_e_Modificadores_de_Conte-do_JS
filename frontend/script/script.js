7a8f5be5139b434d9c1e387ea862c4901b786b0c
LuanLima-Goulart <luanlimaoutlookcom@gmail.com>
2026-08-25 21:48:08 -0300

    Fazendo a 4

---
+++ frontend/script/script.js
@@ -0,0 +1,13 @@
+// Questão 1
+const tituloTag = document.querySelector("h1");
+console.log(tituloTag);
+
+// Questão 2
+const subtitulo = document.getElementsByClassName("subtitulo");
+console.log(subtitulo[0].textContent);
+
+// Questão 3
+const produtos = document.querySelectorAll(".produto");
+console.log(produtos.length);
+
+// Questão 4
